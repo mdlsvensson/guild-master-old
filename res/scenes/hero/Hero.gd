85ef80ca2328965a08a2567cd047bb94f8b342cf
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-10-23 13:37:07 +0200

    Create Hero.gd

---
+++ res/scenes/hero/Hero.gd
@@ -0,0 +1,14 @@
+extends Selectable
+class_name Hero
+func get_class() -> String: return 'Hero'
+
+func _init() -> void:
+  pass
+
+onready var member_3
+
+func _process(delta: float) -> void:
+  pass
+
+func _physics_process(delta: float) -> void:
+  pass
