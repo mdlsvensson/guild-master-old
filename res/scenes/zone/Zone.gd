2597c9460802353b05116a65e14ea2862ed1792b
Wilmer Lindau <38759823+mdlsvensson@users.noreply.github.com>
2022-10-26 19:32:31 +0200

    Added nodepath export, biome setup function

--- res/scenes/zone/Zone.gd
+++ res/scenes/zone/Zone.gd
@@ -1,11 +1,27 @@
-extends Node
-class_name Template#, 'res://editor_icons/icon.svg'
-func get_class() -> String: return 'Template'
+extends Selectable
+class_name Zone
+func get_class() -> String: return 'Zone'
 
-onready var world: Node2D = get_tree().get_root().get_child(0)
+export var navigation_polygon_node: NodePath
+
+var navigation_polygon_instance
+var biome: String
+var terrain: String
+var move_mod: float
+
+onready var hex_sprite: Sprite = $HexSprite
 
 func _ready() -> void:
-	connect("input_event", self, "_on_Zone_input_event")
+	navigation_polygon_instance = get_node_or_null(navigation_polygon_node)
+	if !navigation_polygon_instance: printerr('No navigation polygon for %s' % get_script())
 
-func _on_Zone_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	pass # Replace with function body.
+func setup_biome(biome_type: String, terrain_type: String) -> void:
+	biome = biome_type
+	terrain = terrain_type
+
+	if biome == 'Grass': move_mod = 1
+
+	if terrain == 'Forest Edge': move_mod *= 0.95
+	if terrain == 'Forest' or terrain == 'Mountain Edge': move_mod *= 0.8
+	if terrain == 'Mountain': move_mod *= 0.5
+
